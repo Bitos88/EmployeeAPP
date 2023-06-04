7bc0bd84e9280ab61aa39b5aec0c9e5de0c3c763
Bito <tyrion72@gmail.com>
2023-06-04 09:44:20 +0200

    Initial Commit

---
+++ EmployeeApp/EmployeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeAppApp.swift
+//  EmployeeApp
+//
+//  Created by Alberto Alegre Bravo on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
